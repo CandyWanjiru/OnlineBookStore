92de1880c49be8d0c8d1f0d60b12a93d6eafec72
CandyWanjiru <candy.test@strathmore.edu>
2026-06-19 11:08:20 +0300

    Add dark mode toggle button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("Book Haven Website Loaded!");
+const themeBtn = document.getElementById("themeBtn");
+
+if (themeBtn) {
+
+    themeBtn.addEventListener("click", function () {
+
+        document.body.classList.toggle("dark-mode");
+
+        if (document.body.classList.contains("dark-mode")) {
+            themeBtn.textContent = "☀️ Light Mode";
+        } else {
+            themeBtn.textContent = "🌙 Dark Mode";
+        }
+
+    });
+
+}
