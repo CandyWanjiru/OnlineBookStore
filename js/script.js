814a60e5745d16f04a40c2592a8031873c46f768
CandyWanjiru <candy.test@strathmore.edu>
2026-06-19 11:27:09 +0300

    Implement second JavaScript feature

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
 const themeBtn = document.getElementById("themeBtn");
 
 if (themeBtn) {
-
     themeBtn.addEventListener("click", function () {
-
         document.body.classList.toggle("dark-mode");
 
         if (document.body.classList.contains("dark-mode")) {
-            themeBtn.textContent = "☀️ Light Mode";
+            themeBtn.innerHTML = "☀️ Light Mode";
         } else {
-            themeBtn.textContent = "🌙 Dark Mode";
+            themeBtn.innerHTML = "🌙 Dark Mode";
         }
-
     });
+}
 
+const form = document.getElementById("contactForm");
+
+if (form) {
+    form.addEventListener("submit", function (event) {
+
+        const name = document.getElementById("name").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const message = document.getElementById("message").value.trim();
+
+        if (name === "" || email === "" || message === "") {
+            event.preventDefault();
+            alert("Please fill in all fields.");
+        }
+    });
 }
